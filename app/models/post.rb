dcf031d5bb2f2ac2d40d5639ee65534b95dd1a7a
Ambar Khare <ambar.khare24@gmail.com>
2022-07-11 09:46:08 -0700

    added post controller and model (set up associations)

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :location
+    has_many :favorites
+    has_many :post_categories
+    has_many :categories, through: :post_categories
+end
