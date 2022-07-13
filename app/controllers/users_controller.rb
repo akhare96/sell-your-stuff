d4414a6d44f8803ac9b08dde00ce1ae4fde82d6a
Ambar Khare <ambar.khare24@gmail.com>
2022-07-13 11:33:13 -0700

    added create route that creates new user using strong params and creating session id if user input is valid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if user.save
+            session[:user_id] = @user.id
+            flash[:valid_new] = "Account successfully created"
+            #redirect_to posts_path
+        else
+            flash[:invalid_new] = "Failed to create a new account"
+            #redirect_to new_user_path
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :phone_number, :password, :password_confirmation)
+    end
+
 end
