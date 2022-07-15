b7ecef0fe9c65667d38641b8dff79d12de170384
Ambar Khare <ambar.khare24@gmail.com>
2022-07-14 22:04:05 -0700

    added methods that set session id and redirect user if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,20 @@ class ApplicationController < ActionController::Base
 
     private
 
+    def set_session(user)
+        session[:user_id] = user.id
+    end
+
     def logged_in?
         !!current_user
     end
 
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def verify_logged_in
+        redirect_to root_path unless logged_in?
     end
 
 end
