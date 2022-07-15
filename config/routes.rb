e959afac86461b49b2661d16916067547baef66a
Ambar Khare <ambar.khare24@gmail.com>
2022-07-14 19:19:21 -0700

    added app routes for models, logging in/out, and session id

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root 'application#homepage'
+
+  resources :users, only: [:new, :create, :show, :edit, :update] do
+    resources :posts, only: [:show, :index]
+    resources :favorites, only: [:index]
+  end
+
+  resources :posts
+
+  get '/signup', to: 'users#new', as: 'signup'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  get '/auth/github/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
 end
