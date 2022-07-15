013e839e0cad03a623fe993b8734afc27502242b
Ambar Khare <ambar.khare24@gmail.com>
2022-07-15 15:20:05 -0700

    added route for user to edit own attributes - uses verify_correct_user to make sure user logged in same as user being edited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :verify_logged_in, only: [:edit, :update]
     
     def new
         @user = User.new
@@ -6,17 +7,30 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
-        if user.save
-            session[:user_id] = @user.id
+        if @user.save
+            set_session(@user)
             flash[:valid_new] = "Account successfully created"
-            #redirect_to posts_path
+            redirect_to posts_path
         else
             flash[:invalid_new] = "Failed to create a new account"
-            #redirect_to new_user_path
+            render :new
         end
     end
 
+    def edit
+        if verify_correct_user
+            @user = User.find(params[:id])
+        else
+            redirect_to posts_path
+        end
+    end
+
+
     private
+
+    def verify_correct_user
+        current_user.id == User.find(params[:id])
+    end
 
     def user_params
         params.require(:user).permit(:username, :email, :phone_number, :password, :password_confirmation)
