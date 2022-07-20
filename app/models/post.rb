13a6c13431df32f4ada9646f5765e8c344ebe775
Ambar Khare <ambar.khare24@gmail.com>
2022-07-19 21:27:59 -0700

    made location attribute optional so doesn't give errors if user tries to create account without location but will give errors if trying to create post without location

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
     has_many :post_categories
     has_many :categories, through: :post_categories
     validates :name, :price, :description, :condition, :quantity, presence: true #:show_phone, :phone_calls, :phone_texts, :show_email
-    accepts_nested_attributes_for :location
+    accepts_nested_attributes_for :location, reject_if: :all_blank
 
     def self.conditions
         condition_types = ["new", "like new", "great", "good", "fair"]
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
     has_many :posts
     has_many :categories, through: :posts
     has_many :favorites
+    belongs_to :location, class_name: "Location", optional: true # use when associated record doesn't exist and you're trying to save
     validates :username, uniqueness: { case_sensitive: false }
     validates :username, presence: true
+    accepts_nested_attributes_for :location, reject_if: :all_blank # does not save location if attributes field blank
 end
