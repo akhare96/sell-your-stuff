d9088637c527007bda14cda7bf15c8961ade89b0
Ambar Khare <ambar.khare24@gmail.com>
2022-07-19 20:46:31 -0700

    added attributes for item, boolean default values, and seed file that contains data for categories

--- db/migrate/002_create_posts.rb
+++ db/migrate/002_create_posts.rb
@@ -1,11 +1,18 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
     def change
         create_table :posts do |t|
-        t.string :product_details
-        t.boolean :show_phone
-        t.boolean :phone_calls
-        t.boolean :phone_texts
-        t.boolean :show_email
+        t.string :name
+        t.integer :price
+        t.string :description
+        t.string :make_manufacturer
+        t.string :model_number
+        t.string :size_dimensions
+        t.string :condition
+        t.integer :quantity
+        t.boolean :show_phone, default: false
+        t.boolean :phone_calls, default: false
+        t.boolean :phone_texts, default: false
+        t.boolean :show_email, default: false
         t.integer :user_id
         t.integer :location_id
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+categories = [
+    "Clothing",
+    "Shoes",
+    "Jewelery",
+    "Watches",
+    "Books",
+    "Smart Home",
+    "Home",
+    "Garden",
+    "Tools",
+    "Pet Supplies",
+    "Beauty",
+    "Health",
+    "Toys",
+    "Kids/Baby",
+    "Handmade",
+    "Sports",
+    "Outdoors",
+    "Automotive",
+    "Computers",
+    "Entertainment",
+    "Antique",
+    "Phones"]
+
+categories.each do |name|
+    Category.create(name: name)
+end
