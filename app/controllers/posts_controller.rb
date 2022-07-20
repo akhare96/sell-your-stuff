d6f6c0d1eb73bac4af25c34a5f35d5705fe3a554
Ambar Khare <ambar.khare24@gmail.com>
2022-07-19 21:46:19 -0700

    added callbacks + strong params + verify_user_owns_post to make sure current user can edit owned post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+    before_action :verify_logged_in, only: [:new, :create, :edit, :update]
+    before_action :verify_user_owns_post, only: [:edit, :update]
+
+
+    private
+
+    def verify_user_owns_post
+        current_user.posts.include?(@post)
+    end
+
+    def post_params
+        params.require(:post).permit(:name, :price, :description, :make_manufacturer, :model_number, :size_dimensions, :condition, :quantity, :show_phone, :phone_calls, :phone_texts, :show_email, location_attributes: [:state, :city], category_ids:[])
+    end
 end
