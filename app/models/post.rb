ff271d94541e655de4507c591a4d499703674397
Ambar Khare <ambar.khare24@gmail.com>
2022-07-19 21:16:15 -0700

    added validations and accepts_nested_attributes_for locations so app can produce nested form field + added conditions array for items

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,11 @@ class Post < ActiveRecord::Base
     has_many :favorites
     has_many :post_categories
     has_many :categories, through: :post_categories
-    validates :show_phone, :phone_calls, :phone_texts, :show_email, presence: true
+    validates :name, :price, :description, :condition, :quantity, presence: true #:show_phone, :phone_calls, :phone_texts, :show_email
+    accepts_nested_attributes_for :location
+
+    def self.conditions
+        condition_types = ["new", "like new", "great", "good", "fair"]
+    end
+   
 end
