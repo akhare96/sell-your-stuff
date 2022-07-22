d8ceffb6ace4fb92549772a30fe42a0015935f46
Ambar Khare <ambar.khare24@gmail.com>
2022-07-21 23:50:49 -0700

    added helper method date created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     helper_method :logged_in?
     helper_method :current_user
+    helper_method :date_created
 
     def homepage
         redirect_to posts_path if logged_in?
@@ -28,6 +29,10 @@ class ApplicationController < ActionController::Base
     def location_attributes=(location)
         self.location = Location.find_or_create_by(state: location[:state], city: location[:city])
         self.location.update(location)
+    end
+
+    def date_created(post)
+        post.created_at.strftime("%b %e, %Y")
     end
 
 end
