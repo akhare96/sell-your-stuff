01d71e8984b521feec93ee566d59ea38031140c2
Ambar Khare <ambar.khare24@gmail.com>
2022-07-22 21:42:45 -0700

    added index, create, and destroy method for favorites model + used current_user in create and destroy to ensure favorites belong to user

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,20 @@ class FavoritesController < ApplicationController
     before_action :verify_logged_in
     before_action :user_owns_favorites, only: [:index]
 
+    def index
+        @favorites = User.find(params[:user_id]).favorites
+    end
+
+    def create
+        @favorite = current_user.favorites.create(favorite_params)
+        redirect_back fallback_location: root_path
+    end
+
+    def destroy
+        @favorite = current_user.favorites.find(params[:id])
+        @favorite.destroy
+        redirect_back fallback_location: root_path
+    end
 
 
     private
