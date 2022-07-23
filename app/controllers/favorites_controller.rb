adbba2e419be0a487cd1c1e6668aaf31419fea8e
Ambar Khare <ambar.khare24@gmail.com>
2022-07-22 21:40:55 -0700

    set callbacks and made sure a user can only access own favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,13 @@
 class FavoritesController < ApplicationController
+    before_action :verify_logged_in
+    before_action :user_owns_favorites, only: [:index]
+
+
+
+    private
+
+    def user_owns_favorites
+        redirect_to posts_path unless current_user == User.find(params[:user_id])
+    end
+
 end
